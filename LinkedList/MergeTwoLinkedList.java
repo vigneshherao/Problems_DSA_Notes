8658fdf7615d2c325be1a665ab5db9756d33211a
vignesh <vigneshfornavy@gmail.com>
2024-07-01 22:18:04 +0530

    Added merge two linkedlist solution

--- LinkedList/MergeTwoLinkedList.java
+++ LinkedList/MergeTwoLinkedList.java
@@ -1,3 +1,13 @@
+
+
+//Intution
+//what i did is we can create a new node with dummy value as -1 for itearating;
+// i assinged it for temp node; will iterate through list1 and list2 will gets null
+//check if left is smaller or right is smaller
+//return after sorting with .next becuase delete -1 ele in node
+
+
+
 // class Solution {
 //     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 //         ListNode mergedLinkedList = new ListNode(-1);
