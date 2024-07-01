b5153750cde34db49143ec3e0d44ea2c7991dc11
vignesh <vigneshfornavy@gmail.com>
2024-07-01 22:15:33 +0530

    Added merge two linkedlist solution

---
+++ LinkedList/MergeTwoLinkedList.java
@@ -0,0 +1,36 @@
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode mergedLinkedList = new ListNode(-1);
+//         ListNode temp = mergedLinkedList;
+
+//         while(list1!=null && list2!=null){
+//             if(list1.val<=list2.val){
+//                 temp.next = list1;
+//                 list1 = list1.next;
+//                 temp = temp.next;
+
+//             }
+//             else{
+//                 temp.next = list2;
+//                 list2 = list2.next;
+//                 temp = temp.next;
+//             }
+//         }
+
+//         while(list1!=null){
+//              temp.next = list1;
+//              list1 = list1.next;
+//              temp = temp.next;
+//         }
+
+//         while(list2!=null){
+//              temp.next = list2;
+//              list2 = list2.next;
+//              temp = temp.next;
+//         }
+
+
+//         return mergedLinkedList.next;
+
+//     }
+// }
